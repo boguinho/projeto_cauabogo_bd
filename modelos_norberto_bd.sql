0cf6ed79c49d7b735da4e6c821acff5a95a99989
boguinho <160252917+boguinho@users.noreply.github.com>
2024-06-28 12:08:49 -0300

    Add files via upload

--- modelos_norberto_bd.sql
+++ modelos_norberto_bd.sql
@@ -3,10 +3,12 @@ create database prova;
 use prova;
 
 create table equipamento(
-pk int,
+pk int not null, primary key(pk),
 nome varchar(255) not null,
 setor varchar(255) not null,
-marca varchar(255) not null
+marca varchar(255) not null,
+fornecedor_fk int not null, foreign key(fornecedor_fk) references funcionario(pk),
+manutencao_fk int not null, foreign key(manutencao_fk) references manutencao(pk)
 );
 
 
